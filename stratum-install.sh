b99b287717c8cd254f3464f94d7adf06fa70ed9d
mistical2008 <etalko@gmail.com>
2018-03-13 15:17:09 +0300

    Edits for logging system

--- stratum-install.sh
+++ stratum-install.sh
@@ -6,5 +6,5 @@ STRATUM_DIR=/var/stratum
 screen -dmS main $WEB_DIR/main.sh
 screen -dmS loop2 $WEB_DIR/loop2.sh
 screen -dmS blocks $WEB_DIR/blocks.sh
-screen -dmS debug tail -f $LOG_DIR/debug.log
+screen -dmS debug tail -f $LOG_DIR/stratum.log
 screen -dmS neo $STRATUM_DIR/run.sh neo
