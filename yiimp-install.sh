9aca4150c0343684e1f9a45043e362552098aa29
Evgeniy Talko <etalko@gmail.com>
2018-03-19 15:23:04 +0300

    Added stop-daemons script

--- yiimp-install.sh
+++ yiimp-install.sh
@@ -1,4 +1,4 @@
-
+# dfjdfdfjasfjisfjidjfiasjfiosfjisfjiieiru8huhv
 # Donwnloading script and install
 wget https://raw.githubusercontent.com/xavatar/yiimp_install_scrypt/master/install.sh;
 chmod +x install.sh;
