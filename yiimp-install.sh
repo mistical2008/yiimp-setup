61edef223697211e455eccebc757d1a928a74240
Evgeniy Talko <etalko@gmail.com>
2018-03-21 10:05:48 +0300

    added some aliases and .tmux.conf settings

--- yiimp-install.sh
+++ yiimp-install.sh
@@ -1,3 +1,38 @@
+# Some improvements
+echo 'alias psg="ps aux | grep -v grep | grep -i -e VSZ -e"' >> ~/.bashrc
+cat <<EOF> ~/.tmux.conf
+# Point out to powerline
+source /usr/lib/python3.6/site-packages/powerline/bindings/tmux/powerline.conf
+
+# Set colors support
+set -g default-terminal "xterm-256color"
+
+# set mouse on
+set -g mouse on
+
+# Set hook for continuum plugin
+set -g @continuum-restore 'on'
+
+# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+
+# Other examples:
+# set -g @plugin 'github_username/plugin_name'
+# set -g @plugin 'git@github.com/user/plugin'
+# set -g @plugin 'git@bitbucket.com/user/plugin'
+
+set -g @plugin 'tmux-plugins/tmux-resurrect'
+set -g @plugin 'tmux-plugins/tmux-continuum'
+
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run '~/.tmux/plugins/tpm/tpm'
+EOF
+
+powerline-config tmux setup;
+powerline-daemon --replace;
+
+
 # Donwnloading script and install
 wget https://raw.githubusercontent.com/xavatar/yiimp_install_scrypt/master/install.sh;
 chmod +x install.sh;
