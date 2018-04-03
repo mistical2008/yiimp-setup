3b0663a9a200d96fa35741f8c8bcca5357de4112
Evgeniy Talko <etalko@gmail.com>
2018-04-03 09:34:24 +0300

    Added libdb 4.8-dev

--- server-setup.sh
+++ server-setup.sh
@@ -3,6 +3,9 @@
 apt-get update;
 yes | apt-get upgrade;
 yes | apt-get autoremove;
+sudo apt-get install software-properties-common;
+sudo add-apt-repository ppa:bitcoin/bitcoin;
+sudo apt-get update;
 yes | apt-get install pip powerline libdb4.8-dev libdb4.8++ libdb4.8 libdb4.8++-dev;
 pip install --upgrade pip;
 echo 'open and detach tmux then input this command: "powerline-config tmux setup""';
