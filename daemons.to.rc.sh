0f814e370323741e924cc170b687c37c53a17e85
Evgeniy Talko <etalko@gmail.com>
2018-05-02 16:46:13 +0300

    some

--- daemons.to.rc.sh
+++ daemons.to.rc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-echo "ENTER DAEMONS KEYWORD DEVIDED WITH SPACES"
-read -p "daemons array: " arr
+echo "ENTER DAEMONS KEYWORD"
+echo "In the end type 'Ctrl+D'"
+while read line
+do
+    arr=("${arr[@]}" $line)
+done
+echo =========================================================
+echo DAEMONS LIST
+echo =========================================================
+echo ""
 for i in ${arr[*]}; do ls /usr/bin | grep $i; done | grep "d$";
+echo ""
+echo =========================================================
 echo "COPY LIST ABOVE AND INSERT TO /etc/rc.local"
