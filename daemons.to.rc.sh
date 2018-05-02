ed4ef96b16f284740a6cfce9219aad9fd9ef4674
Evgeniy Talko <etalko@gmail.com>
2018-05-02 16:29:55 +0300

    search daemons in /usr/bin for rc.local

---
+++ daemons.to.rc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "ENTER DAEMONS KEYWORD DEVIDED WITH SPACES"
+read -p "daemons array: " $arr
+for i in ${arr[*]}; do ls /usr/bin | grep $i; done | grep "d$";
+echo "COPY LIST ABOVE AND INSERT TO /etc/rc.local"
